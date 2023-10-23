336010b8e779821c188a2c757de2f828f2f7665a
ArmandoxxXD <mamojose58@gmail.com>
2023-10-23 01:27:49 -0600

    complete integracion de endpoints modulo ingreso

--- src/app/Componentes/home/home.component.ts
+++ src/app/Componentes/home/home.component.ts
@@ -1,9 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HomeService } from 'src/app/Servicios/home.service';
+import { IngresoService } from 'src/app/Servicios/ingreso.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+
+  totalIngreso: number = 0;
+  totalGastos: number = 0;
+  Diferencias: number = 0;
+
+  constructor(private ingresoService:IngresoService,private homeService:HomeService) {
+  }
+
+  ngOnInit() {
+    this.getTotales();
+    
+    this.homeService.currentUpdate.subscribe(updated => {
+      if (updated) {
+        this.getTotales();
+        this.homeService.notifyUpdate(false); // resetear el estado de actualización
+      }
+    });
+  }
+  
+  getTotales() {
+    this.ingresoService.obtenerIngresosTotales().subscribe(
+      data => {
+        this.totalIngreso = data.suma_total_mes;
+      },
+      error => {
+        console.error('Error obteniendo los reportes:', error);
+      }
+    );
+  }
+
 }
