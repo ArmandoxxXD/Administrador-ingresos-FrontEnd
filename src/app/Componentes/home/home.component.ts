e8a5180c277ae5205a3275c26de63e0bc2b01bdf
Osmar Villegas <osmarvm25@gmail.com>
2023-10-29 18:25:08 -0600

    Mackup Gestion finalizado

--- src/app/Componentes/home/home.component.ts
+++ src/app/Componentes/home/home.component.ts
@@ -3,6 +3,7 @@ import { forkJoin } from 'rxjs';
 import { GastoService } from 'src/app/Servicios/gasto.service';
 import { HomeService } from 'src/app/Servicios/home.service';
 import { IngresoService } from 'src/app/Servicios/ingreso.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Observable } from 'rxjs';
 import * as echarts from 'echarts';
 
@@ -13,39 +14,88 @@ import * as echarts from 'echarts';
 })
 export class HomeComponent implements OnInit {
 
+  esModoOscuro: boolean = false;
   totalIngreso: number = 0;
-  esModoOscuro:boolean = false;
   total: number = 0;
   totalGastos: number = 15600;
+
+  totalFormato: String = "";
+  ingresoFormato: String = "";
+  gastoFormato: String = "";
+
+  // Variables formulario
+  switchDiario: boolean = false;
+  miFormulario: FormGroup;
+  reportes: any;
 
   // Variable graficas
   chartDom: any
   myChart: any
 
+
   constructor
     (
       private ingresoService: IngresoService,
       private gastosService: GastoService,
-      private homeService: HomeService
+      private homeService: HomeService,
+      private fb: FormBuilder,
     ) {
-
+    this.miFormulario = this.fb.group({
+      fechaInicio: [null, Validators.required],
+      fechaFin: [null, Validators.required],
+    }, { validators: this.dateRangeValidator });
   }
 
   ngOnInit() {
-    
+
     this.homeService.esModoOscuro$.subscribe((modoOscuro) => {
       this.esModoOscuro = modoOscuro;
     });
 
     this.getTotales();
-    this.modoOscuro();
+
 
     this.homeService.currentUpdate.subscribe(updated => {
       if (updated) {
         this.getTotales();
+        this.modoOscuro();
         this.homeService.notifyUpdate(false); // resetear el estado de actualización
       }
     });
+  }
+
+  dateRangeValidator(group: FormGroup): { [key: string]: any } | null {
+    const fechaInicioControl = group.get('fechaInicio');
+    const fechaFinControl = group.get('fechaFin');
+
+    if (fechaInicioControl && fechaFinControl) {
+      const fechaInicio = fechaInicioControl.value;
+      const fechaFin = fechaFinControl.value;
+
+      if (fechaInicio && fechaFin && fechaInicio > fechaFin) {
+        return { dateRangeInvalid: true };
+      }
+    }
+
+    return null;
+  }
+
+
+  filtrarRegistros() {
+    const fechaInicio = this.miFormulario?.get('fechaInicio')?.value;
+    const fechaFin = this.miFormulario.get('fechaFin')?.value;
+
+    this.ingresoService.obtenerReportesPorMes(fechaInicio, fechaFin).subscribe(
+      data => {
+        this.reportes = data;
+
+
+      },
+      error => {
+        console.error('Error obteniendo los reportes:', error);
+      }
+    );
+
   }
 
   getTotales() {
@@ -55,14 +105,23 @@ export class HomeComponent implements OnInit {
     }).subscribe(result => {
       this.totalIngreso = result.ingreso.suma_total_mes;
       this.totalGastos = result.gasto.suma_total_mes;
+       
 
       // Aquí es donde debes realizar el cálculo
       this.total = this.totalIngreso - this.totalGastos;
       this.actualizarGrafica();
+      this.ingresoFormato = this.formatoDinero(this.totalIngreso);
+      this.gastoFormato = this.formatoDinero(this.totalGastos);
+      this.totalFormato = this.formatoDinero(this.total);
     },
       error => {
         console.error('Error obteniendo los reportes:', error);
       });
+  }
+
+  formatoDinero(value: any) {
+    value = parseInt(value);
+    return '$ ' + value.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
   }
 
   modoOscuro() {
